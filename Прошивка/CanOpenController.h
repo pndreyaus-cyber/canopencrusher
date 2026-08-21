b231194a2a09d96949c00a688bce2286b17a7f61
pndreyaus-cyber <pndreyaus@gmail.com>
2026-08-21 23:19:10 +0300

    Delete Прошивка directory
    It is the same folder as CANCrusher

--- Прошивка/CanOpenController.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "MoveControllerBase.h"
-#include "Axis.h"
-
-using Axis = StepDirController::Axis;
-using MoveController = StepDirController::MoveControllerBase;
--- Прошивка/DebugConfig.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// Uncomment one line to enable debug output and select groups/level.
-
-#define DEBUG_CONFIG DBG_CONFIG(DBG_LEVEL_VERBOSE, DBG_GROUP_MOVE) 
